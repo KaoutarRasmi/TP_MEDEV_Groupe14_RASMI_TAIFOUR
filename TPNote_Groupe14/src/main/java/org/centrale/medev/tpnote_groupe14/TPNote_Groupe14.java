52eb6a5da3e32147f16b1983699c6cbc30da18f3
kaoutar <kaoutarrasmi2002@gmail.com>
2025-01-12 23:14:35 +0100

    Initialisation du projet

---
+++ TPNote_Groupe14/src/main/java/org/centrale/medev/tpnote_groupe14/TPNote_Groupe14.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.centrale.medev.tpnote_groupe14;
+
+/**
+ *
+ * @author kaoutar
+ */
+public class TPNote_Groupe14 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
