ba6fade03669b42c3211aee1dd4d7b5f3c15b8de
kaoutar <kaoutarrasmi2002@gmail.com>
2025-01-13 08:49:23 +0100

    création des méthodes de la classe pion

--- TPNote_Groupe14/src/main/java/org/centrale/medev/tpnote_groupe14/Pion.java
+++ TPNote_Groupe14/src/main/java/org/centrale/medev/tpnote_groupe14/Pion.java
@@ -5,9 +5,50 @@
 package org.centrale.medev.tpnote_groupe14;
 
 /**
- *
+ * Pion 
  * @author kaoutar, mouad
  */
 public class Pion {
+    //Attribus de la classe
+    /**
+     * p: position du pion
+     * couleur: couleur du pion
+     */
+    private Position p;
+    private char couleur;
+    
+    //constructeur 
+
+    public Pion(Position p, char couleur) {
+        this.p = p;
+        this.couleur = couleur;
+    }
+
+    public Pion(Position p) {
+        this.p = p;
+    }
+
+    public Pion(char couleur) {
+        this.couleur = couleur;
+    }
+    
+    
+    //getter et setter
+    public Position getP() {
+        return p;
+    }
+
+    public void setP(Position p) {
+        this.p = p;
+    }
+
+    public char getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(char couleur) {
+        this.couleur = couleur;
+    }
+    
     
 }
